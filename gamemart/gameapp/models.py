2c77146ef5c82232e52ac3eb95a17b6d8c5f27e5
Kamal <mustafakamal87@gmail.com>
2017-01-14 13:21:59 +0200

    User and Game model

--- gamemart/gameapp/models.py
+++ gamemart/gameapp/models.py
@@ -14,3 +14,22 @@ class Review(models.Model):
     user_id = models.ForeignKey(User)
     rating = models.IntegerField()
     review = models.TextField()
+
+class User(models.Model):
+    pic = models.ForeignKey(Asset)
+    name = model.CharField(max_length=50)
+    email = model.EmailField()
+    password = model.CharField(max_length=512)
+    bio = model.TextField()
+    register_date = models.DateTimeField()
+    last_login = models.DateTimeField()
+    user_type = model.CharField(max_length=10) # player, developer, admin
+    is_validated = model.BooleanField()
+
+class Game(models.Model):
+    owner_id = models.ForeignKey(User)
+    title = model.CharField(max_length=100)
+    desc = model.TextField()
+    instruction = model.TextField()
+    url = model.URLField()
+    price = model.FloatField()
