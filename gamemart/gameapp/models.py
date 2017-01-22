28431372c3533c7af25ba5641f3825a54e986156
opeskola <opeskola@gmail.com>
2017-01-22 16:26:37 +0200

    model modified

--- gamemart/gameapp/models.py
+++ gamemart/gameapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Taxonomy(models.Model):
     taxonomy_type = models.CharField(max_length=20)
@@ -11,23 +12,30 @@ class Game_Taxonomy(models.Model):
 
 class Review(models.Model):
     game_id = models.ForeignKey('Game')
-    user_id = models.ForeignKey('User')
+    person_id = models.ForeignKey('Person')
     rating = models.IntegerField()
     review = models.TextField()
 
-class User(models.Model):
+""" We extend User model which has the following fields:
+-username
+-email
+-password
+-last_login
+That's why we don't have to write those fields to Person model"""
+class Person(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     pic = models.ForeignKey('Asset', on_delete=models.CASCADE, related_name='assets')
-    name = models.CharField(max_length=50)
-    email = models.EmailField()
-    password = models.CharField(max_length=512)
+    """name = models.CharField(max_length=50)"""
+    """email = models.EmailField()"""
+    """password = models.CharField(max_length=512)"""
     bio = models.TextField()
     register_date = models.DateTimeField()
-    last_login = models.DateTimeField()
+    """last_login = models.DateTimeField()"""
     user_type = models.CharField(max_length=10) # player, developer, admin
     is_validated = models.BooleanField()
 
 class Game(models.Model):
-    owner_id = models.ForeignKey('User')
+    owner_id = models.ForeignKey('Person')
     title = models.CharField(max_length=100)
     desc = models.TextField()
     instruction = models.TextField()
@@ -35,20 +43,20 @@ class Game(models.Model):
     price = models.FloatField()
 
 class Asset(models.Model):
-    asset_id = models.ForeignKey('User')
+    asset_id = models.ForeignKey('Person')
     asset_type = models.CharField(max_length=50)
     url = models.URLField()
     owner_id = models.ForeignKey('Game')
 
 class Gameplay(models.Model):
-    player_id = models.ForeignKey('User')
+    player_id = models.ForeignKey('Person')
     game_id = models.ForeignKey('Game')
     score = models.FloatField()
     state = models.CharField(max_length=10)
     timestamp = models.DateTimeField(auto_now=True)
 
 class Purchase(models.Model):
-    buyer_id = models.ForeignKey('User')
+    buyer_id = models.ForeignKey('Person')
     game_id = models.ForeignKey('Game')
     date = models.DateTimeField(auto_now=True)
     amount = models.FloatField()
