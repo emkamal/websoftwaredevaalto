8c3bb4508ce412d136c556655ed70db8ddf50748
Dayo Adisa <dayoadisa@gmail.com>
2017-02-02 17:16:11 +0200

    submit game in to db first update

--- gamemart/gameapp/forms.py
+++ gamemart/gameapp/forms.py
@@ -1,5 +1,9 @@
 from django import forms
 from django.contrib.auth.models import User
+
+
+
+
 class UserForm(forms.ModelForm):
   # use password widget so password isn't shown
   password = forms.CharField(widget=forms.PasswordInput())
@@ -9,3 +13,15 @@ class UserForm(forms.ModelForm):
     # ... and the form should have the following fields
     fields = ('first_name', 'last_name', 'username', 'password', 'password', 'email')
     #fields = ('first_name', 'last_name', 'username', 'password', 'password', 'email', 'user_type',)
+
+
+class SubmitForm(forms.ModelForm):
+    class Meta:
+        model = Game # referencing the Game model and its fields
+        fields = [
+            "title",
+            "desc",
+            "instruction",
+            "url",
+            "price"
+        ]
