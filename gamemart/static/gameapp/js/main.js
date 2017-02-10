c6a64a1bf8c747392a96b52f4e85a6b12f4e2f33
Kamal <mustafakamal87@gmail.com>
2017-02-10 16:29:48 +0200

    basic game interaction with the page

--- gamemart/static/gameapp/js/main.js
+++ gamemart/static/gameapp/js/main.js
@@ -12,4 +12,10 @@ $(document).ready(function(){
 
     $('#list').click(function(event){event.preventDefault();$('#products .item').removeClass('grid-group-item');$('#products .item').addClass('list-group-item');});
     $('#grid').click(function(event){event.preventDefault();$('#products .item').removeClass('list-group-item');$('#products .item').addClass('grid-group-item');});
+
+    $(window).on('message', function(evt) {
+      var data = evt.originalEvent.data;
+      console.log("pesan: "+ data);
+    });
+
 });
