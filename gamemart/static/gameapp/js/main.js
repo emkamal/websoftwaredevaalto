dd1eeb705d81d3f712a882ded3c5abda6a696509
Kamal <mustafakamal87@gmail.com>
2017-02-13 16:18:30 +0200

    clean up nav and home.html

--- gamemart/static/gameapp/js/main.js
+++ gamemart/static/gameapp/js/main.js
@@ -1,14 +1,14 @@
 $(document).ready(function(){
-  // $(".dropdown").hover(
-  //     function() {
-  //         $('.dropdown-menu', this).not('.in .dropdown-menu').stop(true,true).slideDown("400");
-  //         $(this).toggleClass('open');
-  //     },
-  //     function() {
-  //         $('.dropdown-menu', this).not('.in .dropdown-menu').stop(true,true).slideUp("400");
-  //         $(this).toggleClass('open');
-  //     }
-  // );
+  $(".dropdown").hover(
+      function() {
+          $('.dropdown-menu', this).not('.in .dropdown-menu').stop(true,true).slideDown("400");
+          $(this).toggleClass('open');
+      },
+      function() {
+          $('.dropdown-menu', this).not('.in .dropdown-menu').stop(true,true).slideUp("400");
+          $(this).toggleClass('open');
+      }
+  );
 
   $('#list').click(function(event){event.preventDefault();$('#products .item').removeClass('grid-group-item');$('#products .item').addClass('list-group-item');});
   $('#grid').click(function(event){event.preventDefault();$('#products .item').removeClass('list-group-item');$('#products .item').addClass('grid-group-item');});
